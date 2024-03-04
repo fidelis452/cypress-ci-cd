12e4981e37cfec5723fb74e15ab08f0a07e05873
Fidelis Wairimu <fidelis.wairimu@acquiretek.com>
2024-03-04 11:45:39 +0300

    cleanup report gen

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,17 +2,8 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
-    charts: true,
-    reportPageTitle: 'custom-title',
-    embeddedScreenshots: true,
-    inlineAssets: true,
-    saveAllAttempts: false,
-    log: true,
-    quest: true,
-  },
   e2e: {
-    baseUrl: 'http://localhost:4200',
+    // baseUrl: 'http://localhost:4200',
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
     },
--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,4 +1,4 @@
-describe('template spec', () => {
+describe('template spec-----', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
   })
